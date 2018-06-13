a1b92b4b1836a6898bcbfa3b6b8ddb4560aafc15
Kelly Olson <kellyolson@northwesternmutual.com>
2018-06-13 18:48:32 -0500

    UI Updates

--- react/src/MatchRetriever.js
+++ react/src/MatchRetriever.js
@@ -1,11 +1,27 @@
 function lookup() {
-    	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function(){
-		if(this.readyState == 4 && this.status == 200){
-			document.getElementById("firstname").innerHTML = this.responseText;
-		}
+
+	var xhttp = new XMLHttpRequest();
+	
+	xhttp.onerror = function() {
+		console.log("Error");
 	};
-    	xhttp.open("GET", "/FindExistingMatches?lanID="+lanID, true);
-    	xhttp.send();
-    	var response = JSON.parse(xhttp.responseText);
+	
+	function ProcessRequest() 
+	{
+	    if (this.readyState == 4 && this.status == 200) {
+            console.log("Response: " + this.responseText);
+       }
+	}
+	
+	var lanID = document.getElementById("lanID").value;
+	var url = "http://localhost:8080/FindExistingMatches?lanID=" + lanID;
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    //xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+    //xhttp.setRequestHeader("Access-Control-Allow-Methods", "GET");
+    //xhttp.setRequestHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,Accept,Origin,Referer,User-Agent");
+    
+    xhttp.onreadystatechange = ProcessRequest;
+    xhttp.send();
+    
 }
