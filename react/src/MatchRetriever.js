99c4390921fb75b2358d9ec7606c19a6708ac0f5
Kelly Olson <kellyolson@northwesternmutual.com>
2018-06-14 08:58:42 -0500

    Hook up UI with backend Spring App

--- react/src/MatchRetriever.js
+++ react/src/MatchRetriever.js
@@ -1,27 +1,21 @@
-function lookup() {
+$(document).ready(function() {
 
-	var xhttp = new XMLHttpRequest();
-	
-	xhttp.onerror = function() {
-		console.log("Error");
-	};
-	
-	function ProcessRequest() 
-	{
-	    if (this.readyState == 4 && this.status == 200) {
-            console.log("Response: " + this.responseText);
-       }
+	$("#lookupButton").on('click', lookup());
+
+	function lookup(lanID) {
+		//e.stopPropagation();
+		var lanID = document.URL.substring(document.URL.length - 5, document.URL.length);
+		var url = "http://localhost:8080/FindExistingMatches?lanID=" + lanID;
+		$.ajax({ 
+			type: "GET",
+			dataType: "json",
+			url: url,
+			success: function(data){        
+			  console.log("Data: " + data);
+			}
+		 });
+
+		console.log("lanID: " + lanID);
 	}
-	
-	var lanID = document.getElementById("lanID").value;
-	var url = "http://localhost:8080/FindExistingMatches?lanID=" + lanID;
-    xhttp.open("GET", url, true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    //xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
-    //xhttp.setRequestHeader("Access-Control-Allow-Methods", "GET");
-    //xhttp.setRequestHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,Accept,Origin,Referer,User-Agent");
-    
-    xhttp.onreadystatechange = ProcessRequest;
-    xhttp.send();
-    
-}
+
+});
