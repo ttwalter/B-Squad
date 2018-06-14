fd36dad3f40228134ac1c2b73fee7e32f96daa5c
FAVRE <jarradfavre@northwesternmutual.com>
2018-06-14 13:42:28 -0500

    Final UI improvements...part 3.

--- react/src/MatchRetriever.js
+++ react/src/MatchRetriever.js
@@ -1,25 +1,6 @@
 $(document).ready(function() {
 
-	$("#lookupButton").on('click', lookup());
 	$("#lunchUpButton").on('click', findMatches());
-
-	function lookup(lanID) {
-		//e.stopPropagation();
-		var lanID = document.URL.substring(document.URL.length - 5, document.URL.length);
-		var url = "http://localhost:8080/GetUserInfo?lanID=" + lanID;
-		$.ajax({ 
-			type: "GET",
-			dataType: "json",
-			url: url,
-			success: function(data){        
-			  console.log("Data: " + data);
-			}
-		 });
-		 
-		 
-
-		console.log("lanID: " + lanID);
-	}
 	
 		function findMatches(lanID) {
 		//e.stopPropagation();
